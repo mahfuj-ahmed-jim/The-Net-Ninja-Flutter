5812f5c0302ef700921998e803a6d3363da13451
Jim <jim@Jims-MacBook-Pro.local>
2022-01-18 03:48:05 +0600

    Scaffold Class & AppBar Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Hello Noobs'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Noobs'),
+        centerTitle: false,
+      ),
+      body: Center(
+        child: Text('Hello How are You'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('Click'),
+      ),
+    ),
   ));
 }
