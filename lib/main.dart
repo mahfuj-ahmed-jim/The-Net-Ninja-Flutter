d6a41bbe895bfd2c39a38e92cd1913ad717fb8ad
Jim <jim@Jims-MacBook-Pro.local>
2022-01-18 15:46:18 +0600

    Colors & TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    // root widget
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Hello Noobs'),
-        centerTitle: false,
+        title: Text(
+          'Hello Noobs',
+          style: TextStyle(
+            color: Colors.blueAccent,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Hello How are You'),
+        child: Text(
+          'Hello How are You',
+          style: TextStyle(
+            color: Colors.blueAccent,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
         child: Text('Click'),
+        backgroundColor: Colors.black,
       ),
     ),
   ));
